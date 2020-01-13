25a409f1e3567e409aa028efbdc988f31d46f589
Navemics <navemics@gmail.com>
2020-01-13 18:02:38 +0530

    Initial Commit

---
+++ tio beta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tio beta
+//
+//  Created by NAVEEN MADHAN on 1/13/20.
+//  Copyright © 2020 Navemics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
